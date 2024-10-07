197d339d186b2f04fe33d8e0a4a036071dea39fa
fxgvcv <gabriel.fogaca@edu.pucrs.br>
2024-10-07 12:11:02 -0300

    Add: Cada código de disciplina é único. Cada código de turma deve ser único para uma mesma disciplina.

--- src/main/java/com/example/resource_management/domain/Turma.java
+++ src/main/java/com/example/resource_management/domain/Turma.java
@@ -1,13 +1,11 @@
 package com.example.resource_management.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"codigo", "disciplina_id"})})
 public class Turma {
 
     @Id
@@ -16,15 +14,20 @@ public class Turma {
 
     private int codigo;
     private String horario;
-
+    
     private String professor;
+
+    @ManyToOne
+    @JoinColumn(name = "disciplina_id", nullable = false)
+    private Disciplina disciplina;
 
     public Turma() {}
 
-    public Turma(int codigo, String horario, String professor) {
+    public Turma(int codigo, String horario, String professor, Disciplina disciplina) {
         this.codigo = codigo;
         this.horario = horario;
         this.professor = professor;
+        this.disciplina = disciplina;
     }
 
 }
