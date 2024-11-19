97a09583fae8075b66fb1ac7fde5cd2f91955ad0
Lucas Ribeiro Sampaio <160547725+lucas-software@users.noreply.github.com>
2024-11-19 21:38:46 +0000

    ref: tests

--- src/test/java/com/example/resource_management/ResourceManagementApplicationTests.java
+++ src/test/java/com/example/resource_management/ResourceManagementApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.resource_management;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class ResourceManagementApplicationTests {
 
--- src/test/java/com/example/resource_management/TurmaServiceTest.java
+++ src/test/java/com/example/resource_management/TurmaServiceTest.java
@@ -18,6 +18,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+
 @ExtendWith(MockitoExtension.class)
 public class TurmaServiceTest {
 
